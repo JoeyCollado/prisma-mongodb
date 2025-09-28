03a2eec07fadac4538822f0f65d636c4a88956c1
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-28 12:23:30 +0800

    building api routes with server actions

---
+++ api-routes-w-serverActions/app/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
+
+//npm i -D prisma = dev dependency prisma
+//npx prisma init
+
+
+//https://cloud.mongodb.com/v2/68bd8455e2c39a0339f6bcc1#/overview
