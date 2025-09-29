cf04f2e7c70a0ad89a6143a242a71def24f0028f
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-29 09:44:00 +0800

    building user interface with server actions

---
+++ user-interface/app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
+
+//npm i -D prisma = dev dependency prisma
+//npx prisma init
+
+
+//https://cloud.mongodb.com/v2/68bd8455e2c39a0339f6bcc1#/overview
+
+//https://www.youtube.com/watch?v=7t_cL2BQ5Ok
